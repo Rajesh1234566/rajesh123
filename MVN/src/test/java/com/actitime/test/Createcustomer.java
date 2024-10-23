4289ac1d58bfb5b81e1789c5e7feda6f0687b9b2
rajes <rajes@DESKTOP-K9R4B6L>
2024-10-23 12:34:37 +0530

    first commit

---
+++ MVN/src/test/java/com/actitime/test/Createcustomer.java
@@ -0,0 +1,13 @@
+package com.actitime.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Createcustomer {
+	@Test
+	public void create()
+	{
+		Reporter.log("create",true);
+	}
+
+}
